af18d33eaeba6657c331ac78d63de7292db61b58
Devin Bost <devin.bost@gmail.com>
2014-05-26 03:58:40 -0700

    Added code for implementing screen controller and getting NiftyGui to actually display screens. (Next, I just need to wire the buttons in the NiftyGui screens.)

---
+++ src/mygame/TestNiftyGui.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ *
+ * @author devinbost
+ */
+//public class TestNiftyGui extends SimpleApplication {
+//  public void simpleInitApp() {
+//     StartScreenState startScreenState = new StartScreenState(this);
+//     stateManager.attach(startScreenState);
+//     // [...] boilerplate init nifty omitted
+//     nifty.fromXml("Interface/myGui.xml", "start", startScreenState); //one of the XML screen elements needs to reference StartScreenState controller class
+//  }
+//}
