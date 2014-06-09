bc2dc0d5113f7d8bf1a4383d83432f7b928e780a
Devin Bost <devin.bost@gmail.com>
2014-06-09 01:26:29 -0700

    Removed unnecessary comments from top.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,46 +1,4 @@
-//package mygame;
-//
-//import com.jme3.app.SimpleApplication;
-//import com.jme3.material.Material;
-//import com.jme3.math.ColorRGBA;
-//import com.jme3.math.Vector3f;
-//import com.jme3.renderer.RenderManager;
-//import com.jme3.scene.Geometry;
-//import com.jme3.scene.shape.Box;
 
-/**
- * test
- * @author Devin Bost
- */
-//public class Main extends SimpleApplication { // This extends the Application class. The Application class represents a generic real-time 3D rendering jME3 application
-//
-//    public static void main(String[] args) {
-//        Main app = new Main();
-//        app.start();
-//    }
-//
-//    @Override
-//    public void simpleInitApp() {
-//        Box b = new Box(1, 1, 1);
-//        Geometry geom = new Geometry("Box", b);
-//
-//        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-//        mat.setColor("Color", ColorRGBA.Red);
-//        geom.setMaterial(mat);
-//
-//        rootNode.attachChild(geom);
-//    }
-//
-//    @Override
-//    public void simpleUpdate(float tpf) {
-//        //TODO: add update code
-//    }
-//
-//    @Override
-//    public void simpleRender(RenderManager rm) {
-//        //TODO: add render code
-//    }
-//}
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
