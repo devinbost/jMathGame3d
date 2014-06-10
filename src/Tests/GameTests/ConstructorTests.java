c2f0484670f8a69085bbb43310a5fc38d094682d
Devin Bost <devin.bost@gmail.com>
2014-06-09 18:52:33 -0700

    Added SharedConstructors_IsConstructorWorking_ReturnsTrue and outline of test methods for testing construction of bricks and brick wall (via shared constructors).

--- src/Tests/GameTests/ConstructorTests.java
+++ src/Tests/GameTests/ConstructorTests.java
@@ -4,19 +4,22 @@
  */
 package Tests.GameTests;
 
+import com.jme3.bullet.BulletAppState;
+import com.jme3.scene.Node;
+import mygame.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
 /**
  *
  * @author devinbost
  */
 public class ConstructorTests {
-    
+   
     public ConstructorTests() {
     }
 //    
@@ -42,6 +45,40 @@ public class ConstructorTests {
     // public void hello() {}
     @Test
     public void SharedConstructors_IsConstructorWorking_ReturnsTrue(){
-        SharedConstructors constructors = new SharedConstructors()
+//        Main mockedMainRootNode = mock(mygame.Main.class);
+//        Node rootNode = mockedMainRootNode.getRootNode();
+//        BulletAppState appState = mockedMainRootNode.getBulletAppState();
+        System.out.println("Running SharedConstructors_IsConstructorWorking_ReturnsTrue()");
+        BulletAppState appState = mock(BulletAppState.class);
+        Node rootNode = mock(Node.class);
+        SharedConstructors constructors = new SharedConstructors(rootNode, appState);
+        assertTrue(constructors instanceof SharedConstructors);
+        
+    }
+    @Test
+    public void SharedConstructors_IsMakeBrickConstructingBrick_ReturnsTrue(){
+//        Main mockedMainRootNode = mock(mygame.Main.class);
+//        Node rootNode = mockedMainRootNode.getRootNode();
+//        BulletAppState appState = mockedMainRootNode.getBulletAppState();
+        System.out.println("Running SharedConstructors_IsConstructorWorking_ReturnsTrue()");
+        BulletAppState appState = mock(BulletAppState.class);
+        Node rootNode = mock(Node.class);
+        SharedConstructors constructors = new SharedConstructors(rootNode, appState);
+        
+        assertTrue(1 == 2);
+        
+    }
+    @Test
+    public void SharedConstructors_IsInitWallConstructingBricks_ReturnsTrue(){
+//        Main mockedMainRootNode = mock(mygame.Main.class);
+//        Node rootNode = mockedMainRootNode.getRootNode();
+//        BulletAppState appState = mockedMainRootNode.getBulletAppState();
+        System.out.println("Running SharedConstructors_IsConstructorWorking_ReturnsTrue()");
+        BulletAppState appState = mock(BulletAppState.class);
+        Node rootNode = mock(Node.class);
+        SharedConstructors constructors = new SharedConstructors(rootNode, appState);
+        
+        assertTrue(1 == 2);
+        
     }
 }
